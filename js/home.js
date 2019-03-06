8f1e49cd2149ea1617c8dcf348f66f69db9197a1
TheWolfBane154 <45442131+TheWolfBane154@users.noreply.github.com>
2019-03-06 15:48:27 -0500

    Create home.js

---
+++ js/home.js
@@ -0,0 +1,8 @@
+//Beta Alert :D
+
+alert("This is in beta!")
+alert("Why are you here?")
+alert("Dedicated are you?")
+alert("Just leave stop wasteing your time.")
+alert("Why are you so dedicated?")
+alert("I might as well just block your IP shouldent I?")
